e895f0e7f7ed3ce6d2aadefda27d6b799359721e
Abhijit Gadgil <gabhijit@iitbombay.org>
2024-03-21 16:08:45 +0530

    Fixed to snake_case and tutorial_name in XDP funcs
    XDP Funcs now use proper snake_case and we are correctly using the
    tutorial_name instead of project name for writing the function names.

--- templates/basic/{{tutorial_name}}-ebpf/src/main.rs
+++ templates/basic/{{tutorial_name}}-ebpf/src/main.rs
@@ -5,14 +5,14 @@ use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
 
 #[xdp]
-pub fn {{name}}(ctx: XdpContext) -> u32 {
-    match try_{{name}}(ctx) {
+pub fn {{to_snake_case tutorial_name}}(ctx: XdpContext) -> u32 {
+    match try_{{to_snake_case tutorial_name}}(ctx) {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-fn try_{{name}}(ctx: XdpContext) -> Result<u32, u32> {
+fn try_{{to_snake_case tutorial_name}}(ctx: XdpContext) -> Result<u32, u32> {
     info!(&ctx, "received a packet");
     Ok(xdp_action::XDP_PASS)
 }
