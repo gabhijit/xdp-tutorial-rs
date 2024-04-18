39fec306253be10a0b4c9a407a112b0dda9ef7f5
Abhijit Gadgil <gabhijit@iitbombay.org>
2024-04-18 16:04:23 +0530

    Initial working `init` subcommand

--- src/init.rs
+++ src/init.rs
@@ -5,14 +5,53 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 pub(crate) struct InitCommand {
     /// A Path to local directory or a git repository containing XDP Tutorial Templates.
-    #[clap(name = "template-path", required = true)]
+    #[clap(name = "template-path")]
     template_path: std::path::PathBuf,
 
+    /// Path inside the root of the repository, if the given template path is a GIT repository.
+    #[clap(name = "repository-template-path", short = 'r', long = "path")]
+    repository_template_path: Option<std::path::PathBuf>,
+
+    /// Git Reference if the template-path is a repository (branch, tag or commit hash)
+    #[clap(name = "git-ref", short = 't')]
+    git_ref: Option<String>,
+
     /// A Path to local directory where the templates should be instantiated
-    #[clap(name = "tutorial-path", default_value = ".")]
-    tutorial_path: std::path::PathBuf,
+    #[clap(name = "tutorial-path", short = 'o', default_value = ".")]
+    tutorial_path: String,
 }
 
-pub(crate) fn do_init(init_command: InitCommand) {
+pub(crate) fn do_init(init_command: InitCommand) -> anyhow::Result<()> {
     eprintln!("Init Command: {init_command:#?}");
+
+    let scaffold_ops = cargo_scaffold::Opts::builder(init_command.template_path);
+
+    let scaffold_ops = if let Some(repository_template_path) = init_command.repository_template_path
+    {
+        scaffold_ops.repository_template_path(repository_template_path)
+    } else {
+        scaffold_ops
+    };
+
+    let scaffold_ops = if let Some(git_ref) = init_command.git_ref {
+        scaffold_ops.repository_template_path(git_ref)
+    } else {
+        scaffold_ops
+    };
+
+    let project_name = if init_command.tutorial_path.as_str() == "." {
+        "xdp-tutorial-rust-sol".into()
+    } else {
+        init_command.tutorial_path
+    };
+
+    let scaffold_ops = scaffold_ops.project_name(project_name);
+
+    eprintln!("scaffold_opts: {scaffold_ops:#?}");
+
+    let scaffold_desc = cargo_scaffold::ScaffoldDescription::new(scaffold_ops)?;
+
+    let params = scaffold_desc.fetch_parameters_value()?;
+
+    scaffold_desc.scaffold_with_parameters(params)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use clap::Parser;
 use init::{do_init, InitCommand};
 
 #[derive(Debug, Parser)]
+struct TestSetup;
+
+#[derive(Debug, Parser)]
+struct AddTutorial;
+
+#[derive(Debug, Parser)]
 struct Cli {
     #[clap(subcommand)]
     command: Command,
@@ -14,13 +20,21 @@ struct Cli {
 
 #[derive(Debug, Parser)]
 enum Command {
+    /// Initialize the project for tutorials.
     Init(InitCommand),
+
+    /// Setup test environment for running the tutorials.
+    TestEnv(TestSetup),
+
+    /// Add individual tutorials to the project.
+    Add(AddTutorial),
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
         Command::Init(i) => do_init(i),
+        _ => todo!(),
     }
 }
