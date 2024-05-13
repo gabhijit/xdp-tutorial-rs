4448612f68366494c5e598b88fb75694ee2994f0
Abhijit Gadgil <gabhijit@iitbombay.org>
2024-05-13 15:51:02 +0530

    First implementation of the `load` functionality
    Using the `EbpfBuilder` to 'customize' loading of the maps and better
    CLI handling for the actions.
    
    Signed-off-by: Abhijit Gadgil <gabhijit@iitbombay.org>

--- templates/basic/basic-04/xdp-runner/src/xdp-runner.rs
+++ templates/basic/basic-04/xdp-runner/src/xdp-runner.rs
@@ -4,33 +4,65 @@ use anyhow::Context;
 
 use aya::maps::{MapData, PerCpuArray};
 use aya::programs::{Xdp, XdpFlags};
-use aya::Ebpf;
+use aya::EbpfLoader;
 use aya_log::EbpfLogger;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use log::{info, warn};
 use tokio::{signal, time};
 
-use {{ to_snake_case tutorial_name }}_common::StatsRecord;
+use {{to_snake_case tutorial_name}}_common::StatsRecord;
 
 #[derive(Debug, Parser)]
-struct Opt {
+enum CliCommand {
+    /// Pin: Pins a given map
+    Pin,
+
+    /// Load: Loads a given Map and
+    Load(LoadCommand),
+
+    /// Unpin: Unpins, Removes a pinned map
+    Unpin,
+
+    /// List: Lists all Pinned Maps
+    List,
+}
+
+#[derive(Debug, Parser)]
+struct LoadCommand {
     #[clap(short, long)]
-    program: String,
+    action: Action,
 
     #[clap(short, long)]
     file: String,
 
     #[clap(short, long, default_value = "lo")]
     iface: String,
+
+    #[clap(long)]
+    release: bool,
 }
 
-async fn print_stats_for_action(stats_array: &PerCpuArray<&MapData, StatsRecord>, action: u32) {
+#[derive(Debug, Clone, Default, ValueEnum)]
+enum Action {
+    #[default]
+    /// Select the XDP_PASS action
+    Pass,
+
+    /// Select the XDP_DROP action
+    Drop,
+}
+
+async fn print_stats_for_action(
+    stats_array: &PerCpuArray<&MapData, StatsRecord>,
+    action: u32,
+    action_name: &str,
+) {
     let values = stats_array.get(&action, 0).unwrap();
     for (i, value) in values.iter().enumerate() {
         info!(
             "CPU: {}, Action: {}, Packet Count: {}",
-            i, action, value.pkt_count
+            i, action_name, value.pkt_count
         );
     }
 }
@@ -43,17 +75,42 @@ async fn print_stats_for_action(stats_array: &PerCpuArray<&MapData, StatsRecord>
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    stats().await
+    let cli = CliCommand::parse();
+
+    match cli {
+        CliCommand::Load(opts) => stats(opts).await,
+        _ => Err(anyhow::Error::msg("Unhandled command")),
+    }
 }
 
-async fn stats() -> anyhow::Result<()> {
-    let opts = Opt::parse();
+async fn stats(opts: LoadCommand) -> anyhow::Result<()> {
     env_logger::init();
 
-    let bpf_bin = format!("target/bpfel-unknown-none/debug/{}", opts.file);
-    let bpf_bin = std::fs::read(&bpf_bin)?;
-    let mut bpf = Ebpf::load(&bpf_bin)?;
-    let xdp_program = bpf.program_mut(&opts.program);
+    let profile = if opts.release { "release" } else { "debug" };
+
+    let bpf_file = format!("target/bpfel-unknown-none/{}/{}", profile, opts.file);
+
+    // We create `map_pin_path` base directory to load the "pinned" maps. The "pinned" maps will be
+    // loaded to "/map/pin/path/<MAP_NAME>" file.
+    let map_pin_path = format!("/sys/fs/bpf/{}/{}/", opts.iface, opts.file);
+
+    // The dirs are rquired to be present for the maps to be loaded.
+    std::fs::create_dir_all(&map_pin_path)?;
+
+    // Instead of Using `Ebpf::load` API, we use the `EbpfLoader` API to customize the loading of
+    // maps. This allows loading of the maps of 'unsupported' types to be loaded. But they won't be
+    // accessible from the userspace (This avoids failure on loading unsupported maps).
+    let mut bpf = EbpfLoader::new()
+        .allow_unsupported_maps()
+        .map_pin_path(map_pin_path)
+        .load_file(bpf_file)?;
+
+    let (action_name, action_val, program) = match opts.action {
+        Action::Pass => ("pass", 2u32, "{{ to_snake_case tutorial_name }}_action_pass"),
+        Action::Drop => ("drop", 1u32, "{{ to_snake_case tutorial_name }}_action_drop"),
+    };
+
+    let xdp_program = bpf.program_mut(program);
 
     if let Some(xdp_program) = xdp_program {
         let xdp: &mut Xdp = xdp_program.try_into()?;
@@ -69,18 +126,20 @@ async fn stats() -> anyhow::Result<()> {
 
         info!(
             "XDP Program '{}' attached to '{}'! Now waiting for Ctrl-C",
-            &opts.program, &opts.iface
+            program, &opts.iface
         );
 
         let mut stats_poller_interval = time::interval(Duration::from_secs(2));
+
+        let map = bpf.map("PINNED_PERCPU_ARRAY").unwrap();
+        let stats_array = PerCpuArray::try_from(map).unwrap();
 
         loop {
             tokio::select! {
                 _ = stats_poller_interval.tick() => {
                     info!("tick!");
-                    let stats_array = PerCpuArray::try_from(bpf.map("PINNED_PERCPU_ARRAY").unwrap()).unwrap();
                     // TODO: Right now just using XDP_PASS, make it a proper action
-                    print_stats_for_action(&stats_array, 2).await;
+                    print_stats_for_action(&stats_array, action_val, action_name).await;
                 }
                 _ = signal::ctrl_c() => {
                     info!("Exiting...");
@@ -98,7 +157,7 @@ async fn stats() -> anyhow::Result<()> {
 
         Err(anyhow::Error::msg(format!(
             "Unable to find the program '{}' in the loaded file '{}'. Available programs are: {}",
-            opts.program,
+            program,
             opts.file,
             progs.join(", "),
         )))
--- templates/basic/basic-04/{{tutorial_name}}-ebpf/src/main.rs
+++ templates/basic/basic-04/{{tutorial_name}}-ebpf/src/main.rs
@@ -14,26 +14,27 @@ use {{ to_snake_case tutorial_name }}_common::StatsRecord;
 const XDP_ACTION_MAX: u32 = xdp_action::XDP_REDIRECT + 1;
 
 #[map]
-static PINNED_PERCPU_ARRAY: PerCpuArray<StatsRecord> = PerCpuArray::<StatsRecord>::pinned(XDP_ACTION_MAX, 0);
+static PINNED_PERCPU_ARRAY: PerCpuArray<StatsRecord> =
+    PerCpuArray::<StatsRecord>::pinned(XDP_ACTION_MAX, 0);
 
 #[xdp]
-pub fn {{to_snake_case tutorial_name}}_action_pass(ctx: XdpContext) -> u32 {
-    match try_{{to_snake_case tutorial_name}}_packet_stats(ctx, xdp_action::XDP_PASS) {
+pub fn {{ to_snake_case tutorial_name }}_action_pass(ctx: XdpContext) -> u32 {
+    match try_{{ to_snake_case tutorial_name }}_packet_stats(ctx, xdp_action::XDP_PASS) {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
 #[xdp]
-pub fn {{to_snake_case tutorial_name}}_action_drop(ctx: XdpContext) -> u32 {
-    match try_{{to_snake_case tutorial_name}}_packet_stats(ctx, xdp_action::XDP_DROP) {
+pub fn {{ to_snake_case tutorial_name }}_action_drop(ctx: XdpContext) -> u32 {
+    match try_{{ to_snake_case tutorial_name }}_packet_stats(ctx, xdp_action::XDP_DROP) {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
 #[inline(always)]
-fn try_{{to_snake_case tutorial_name}}_packet_stats(ctx: XdpContext, action: u32) -> Result<u32, u32> {
+fn try_{{ to_snake_case tutorial_name }}_packet_stats(ctx: XdpContext, action: u32) -> Result<u32, u32> {
     debug!(&ctx, "Received a packet.");
     let record = PINNED_PERCPU_ARRAY.get_ptr_mut(action);
     if let Some(record) = record {
