42972425aac633ef727a9618cb0dcb4ebfe7625b
Abhijit Gadgil <gabhijit@iitbombay.org>
2024-05-16 15:25:03 +0530

    Using separate actions in tutorial 'basic-03'
    - Now the tutorial 'basic-03' takes both actions, 'pass' and 'drop'.
    - Also added log statement in the userspace program
    - Added `#[inline(always)]` attribute to `try_basic_03_packet_stats`

--- templates/basic/basic-03/xdp-runner/src/xdp-runner.rs
+++ templates/basic/basic-03/xdp-runner/src/xdp-runner.rs
@@ -7,16 +7,15 @@ use aya::programs::{Xdp, XdpFlags};
 use aya::Ebpf;
 use aya_log::EbpfLogger;
 
-use clap::Parser;
-use log::{info, warn};
+use clap::{ValueEnum, Parser};
 use tokio::{signal, time};
 
 use {{ to_snake_case tutorial_name }}_common::StatsRecord;
 
 #[derive(Debug, Parser)]
 struct Opt {
-    #[clap(short, long)]
-    program: String,
+    #[clap(long)]
+    action: XdpAction,
 
     #[clap(short, long, default_value = "{{tutorial_name}}")]
     file: String,
@@ -28,9 +27,25 @@ struct Opt {
     release: bool,
 }
 
-async fn print_stats(stats_array: &Array<&MapData, StatsRecord>) {
-    let stats = stats_array.get(&2, 0);
-    info!("Packet Count: {}", stats.unwrap().pkt_count);
+#[derive(Debug, Clone, Default, ValueEnum)]
+enum XdpAction {
+    #[default]
+    /// Select the XDP_PASS action
+    Pass,
+
+    /// Select the XDP_DROP action
+    Drop,
+}
+
+fn action_info_from_opts(opts_action: &XdpAction) -> (&str, u32, &str) {
+    match opts_action {
+        XdpAction::Pass => ("pass", 2u32, "{{to_snake_case tutorial_name}}_pass_packet_stats"),
+        XdpAction::Drop => ("drop", 1u32, "{{to_snake_case tutorial_name}}_drop_packet_stats"),
+    }
+}
+async fn print_stats(stats_array: &Array<&MapData, StatsRecord>, action: u32, action_name: &str) {
+    let stats = stats_array.get(&action, 0);
+    log::info!("Packet Count for action '{}' : {}", action_name, stats.unwrap().pkt_count);
 }
 
 // This is a Userspace program that is responsible for 'installing' the XDP eBPF binary in the
@@ -41,16 +56,22 @@ async fn print_stats(stats_array: &Array<&MapData, StatsRecord>) {
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let opts = Opt::parse();
     env_logger::init();
 
+    let opts = Opt::parse();
+
     let profile = if opts.release { "release" } else { "debug" };
+
     let bpf_bin = format!("target/bpfel-unknown-none/{}/{}", profile, opts.file);
+    log::info!("Loading eBPF file: '{}'", bpf_bin);
+
     let bpf_bin = std::fs::read(&bpf_bin)?;
     let mut bpf = Ebpf::load(&bpf_bin)?;
-    let xdp_program = bpf.program_mut(&opts.program);
 
+    let (action_name, action, program_name) = action_info_from_opts(&opts.action);
+    let xdp_program = bpf.program_mut(program_name);
     if let Some(xdp_program) = xdp_program {
+        log::trace!("Attaching program '{}' to interface '{}'", program_name, opts.iface);
         let xdp: &mut Xdp = xdp_program.try_into()?;
         xdp.load()?;
         let _linkid = xdp
@@ -59,25 +80,27 @@ async fn main() -> Result<(), anyhow::Error> {
 
         if let Err(e) = EbpfLogger::init(&mut bpf) {
             // This can happen if you remove all log statements from your eBPF program.
-            warn!("failed to initialize eBPF logger: {}", e);
+            log::warn!("failed to initialize eBPF logger: {}", e);
         }
 
-        info!(
+
+        log::info!(
             "XDP Program '{}' attached to '{}'! Now waiting for Ctrl-C",
-            &opts.program, &opts.iface
+            program_name, &opts.iface
         );
 
         let mut stats_poller_interval = time::interval(Duration::from_secs(2));
 
+
         loop {
             tokio::select! {
                 _ = stats_poller_interval.tick() => {
-                    info!("tick!");
+                    log::info!("tick!");
                     let stats_array = Array::try_from(bpf.map("STATS_ARRAY").unwrap()).unwrap();
-                    print_stats(&stats_array).await;
+                    print_stats(&stats_array, action, action_name).await;
                 }
                 _ = signal::ctrl_c() => {
-                    info!("Exiting...");
+                    log::info!("Exiting...");
                     break;
                 }
             }
@@ -86,13 +109,13 @@ async fn main() -> Result<(), anyhow::Error> {
         Ok(())
     } else {
         let mut progs = vec![];
-        for (name, _program_type) in bpf.programs() {
+        for (name, _) in bpf.programs() {
             progs.push(name);
         }
 
         Err(anyhow::Error::msg(format!(
             "Unable to find the program '{}' in the loaded file '{}'. Available programs are: {}",
-            opts.program,
+            program_name,
             opts.file,
             progs.join(", "),
         )))
--- templates/basic/basic-03/{{tutorial_name}}-ebpf/src/main.rs
+++ templates/basic/basic-03/{{tutorial_name}}-ebpf/src/main.rs
@@ -21,21 +21,30 @@ const XDP_ACTION_MAX: u32 = xdp_action::XDP_REDIRECT + 1;
 static STATS_ARRAY: Array<StatsRecord> = Array::<StatsRecord>::with_max_entries(XDP_ACTION_MAX, 0);
 
 #[xdp]
-pub fn {{to_snake_case tutorial_name}}_packet_stats(ctx: XdpContext) -> u32 {
-    match try_{{to_snake_case tutorial_name}}_packet_stats(ctx) {
+pub fn {{to_snake_case tutorial_name}}_pass_packet_stats(ctx: XdpContext) -> u32 {
+    match try_{{to_snake_case tutorial_name}}_packet_stats(ctx, xdp_action::XDP_PASS) {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-fn try_{{to_snake_case tutorial_name}}_packet_stats(ctx: XdpContext) -> Result<u32, u32> {
+#[xdp]
+pub fn {{to_snake_case tutorial_name}}_drop_packet_stats(ctx: XdpContext) -> u32 {
+    match try_{{to_snake_case tutorial_name}}_packet_stats(ctx, xdp_action::XDP_DROP) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+#[inline(always)]
+fn try_{{to_snake_case tutorial_name}}_packet_stats(ctx: XdpContext, action: u32) -> Result<u32, u32> {
     debug!(&ctx, "Received a packet.");
-    let record = STATS_ARRAY.get_ptr_mut(xdp_action::XDP_PASS);
+    let record = STATS_ARRAY.get_ptr_mut(action);
     if let Some(record) = record {
         let _ = unsafe {
             atomic_xadd_acquire(&mut (*record).pkt_count, 1);
         };
-        Ok(xdp_action::XDP_PASS)
+        Ok(action)
     } else {
         error!(&ctx, "Entry for the action not found in the map!");
         Err(xdp_action::XDP_ABORTED)
